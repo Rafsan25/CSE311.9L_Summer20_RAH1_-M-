a3a8d3a626312a56a5545329cafaecd0d96508d0
syed-reza98 <syed.reza@northsouth.edu>
2020-09-14 12:59:08 +0600

    created config php file

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/*server with default (user 'root' with no password) */
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'mydb');
+
+/*connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+echo ("Connection Established");
+
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
