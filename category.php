09e8441eb8f4a7e6a360cc66a1dbfd66302547ec
syed-reza98 <syed.reza@northsouth.edu>
2020-09-16 21:12:53 +0600

    Made rows for category.php and injected sql. Added grid_title in style.css. Didn't show any data yet

--- category.php
+++ category.php
@@ -1,164 +1,46 @@
-<?php include ('top.php');?>
+<?php
+include ('top.php');
+$sql="SELECT * FROM category ORDER BY order_number";
+$res=mysqli_query($link,$sql);
+
+?>
 <div class="card">
     <div class="card-body">
-        <h4 class="card-title">Data table</h4>
+        <h2 class="grid_title">Category</h2>
         <div class="row">
             <div class="col-12">
                 <div class="table-responsive">
                     <table id="order-listing" class="table">
                         <thead>
                         <tr>
-                            <th>Order #</th>
-                            <th>Purchased On</th>
-                            <th>Customer</th>
-                            <th>Ship to</th>
-                            <th>Base Price</th>
-                            <th>Purchased Price</th>
-                            <th>Status</th>
+                            <th>Serial No</th>
+                            <th>Category</th>
+                            <th>Order Number</th>
                             <th>Actions</th>
                         </tr>
                         </thead>
                         <tbody>
+                        <?php if(mysqli_num_rows($res)>0) {
+                            $i=1;
+                            while($row=mysqli_fetch_assoc($res)){
+
+                            ?>
                         <tr>
-                            <td>1</td>
-                            <td>2012/08/03</td>
-                            <td>Edinburgh</td>
-                            <td>New York</td>
-                            <td>$1500</td>
-                            <td>$3200</td>
-                            <td>
-                                <label class="badge badge-info">On hold</label>
-                            </td>
-                            <td>
-                                <button class="btn btn-outline-primary">View</button>
-                            </td>
+                            <td><?php echo  $i?></td>
+                            <td><? echo $row['category']?></td>
+                            <td><? echo $row['order_number']?></td>
+                            <td></td>
+
                         </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>2015/04/01</td>
-                            <td>Doe</td>
-                            <td>Brazil</td>
-                            <td>$4500</td>
-                            <td>$7500</td>
-                            <td>
-                                <label class="badge badge-danger">Pending</label>
-                            </td>
-                            <td>
-                                <button class="btn btn-outline-primary">View</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>2010/11/21</td>
-                            <td>Sam</td>
-                            <td>Tokyo</td>
-                            <td>$2100</td>
-                            <td>$6300</td>
-                            <td>
-                                <label class="badge badge-success">Closed</label>
-                            </td>
-                            <td>
-                                <button class="btn btn-outline-primary">View</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>4</td>
-                            <td>2016/01/12</td>
-                            <td>Sam</td>
-                            <td>Tokyo</td>
-                            <td>$2100</td>
-                            <td>$6300</td>
-                            <td>
-                                <label class="badge badge-success">Closed</label>
-                            </td>
-                            <td>
-                                <button class="btn btn-outline-primary">View</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>5</td>
-                            <td>2017/12/28</td>
-                            <td>Sam</td>
-                            <td>Tokyo</td>
-                            <td>$2100</td>
-                            <td>$6300</td>
-                            <td>
-                                <label class="badge badge-success">Closed</label>
-                            </td>
-                            <td>
-                                <button class="btn btn-outline-primary">View</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>6</td>
-                            <td>2000/10/30</td>
-                            <td>Sam</td>
-                            <td>Tokyo</td>
-                            <td>$2100</td>
-                            <td>$6300</td>
-                            <td>
-                                <label class="badge badge-info">On-hold</label>
-                            </td>
-                            <td>
-                                <button class="btn btn-outline-primary">View</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>7</td>
-                            <td>2011/03/11</td>
-                            <td>Cris</td>
-                            <td>Tokyo</td>
-                            <td>$2100</td>
-                            <td>$6300</td>
-                            <td>
-                                <label class="badge badge-success">Closed</label>
-                            </td>
-                            <td>
-                                <button class="btn btn-outline-primary">View</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>8</td>
-                            <td>2015/06/25</td>
-                            <td>Tim</td>
-                            <td>Italy</td>
-                            <td>$6300</td>
-                            <td>$2100</td>
-                            <td>
-                                <label class="badge badge-info">On-hold</label>
-                            </td>
-                            <td>
-                                <button class="btn btn-outline-primary">View</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>9</td>
-                            <td>2016/11/12</td>
-                            <td>John</td>
-                            <td>Tokyo</td>
-                            <td>$2100</td>
-                            <td>$6300</td>
-                            <td>
-                                <label class="badge badge-success">Closed</label>
-                            </td>
-                            <td>
-                                <button class="btn btn-outline-primary">View</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>10</td>
-                            <td>2003/12/26</td>
-                            <td>Tom</td>
-                            <td>Germany</td>
-                            <td>$1100</td>
-                            <td>$2300</td>
-                            <td>
-                                <label class="badge badge-danger">Pending</label>
-                            </td>
-                            <td>
-                                <button class="btn btn-outline-primary">View</button>
-                            </td>
-                        </tr>
+                        <?php
+                            $i++;
+                            } } else { ?>
+                            <td colspan="5">No Data Found</td>
+
+
+                        <?php
+                        } ?>
+
                         </tbody>
                     </table>
                 </div>
