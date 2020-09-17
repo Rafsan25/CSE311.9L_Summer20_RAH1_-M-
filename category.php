aa4e7be7ac4b0f5ebcaf23df4ca39069d9af39be
syed-reza98 <syed.reza@northsouth.edu>
2020-09-17 14:22:51 +0600

    Replaced code of category.php. Injected sql. Fetched data from category table

--- category.php
+++ category.php
@@ -1,52 +1,51 @@
 <?php
-include ('top.php');
-$sql="SELECT * FROM category ORDER BY order_number";
+include('top.php');
+$sql="select * from category order by order_number";
 $res=mysqli_query($con,$sql);
 
+
 ?>
-<div class="card">
-    <div class="card-body">
-        <h2 class="grid_title">Category</h2>
-        <div class="row">
-            <div class="col-12">
-                <div class="table-responsive">
-                    <table id="order-listing" class="table">
-                        <thead>
-                        <tr>
-                            <th>Serial No</th>
-                            <th>Category</th>
-                            <th>Order Number</th>
-                            <th>Actions</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <?php if(mysqli_num_rows($res)>0) {
-                            $i=1;
-                            while($row=mysqli_fetch_assoc($res)){
+    <div class="card">
+        <div class="card-body">
+            <h2 class="grid_title">Category</h2>
+            <div class="row">
+                <div class="col-12">
+                    <div class="table-responsive">
+                        <table id="order-listing" class="table">
+                            <thead>
+                            <tr>
+                                <th width="10%">S. No</th>
+                                <th width="45%">Category</th>
+                                <th width="15%">Order Number</th>
+                                <th width="30%">Actions</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php if(mysqli_num_rows($res)>0) {
+                                $i=1;
+                                while($row=mysqli_fetch_assoc($res)){
+                                ?>
+                            <tr>
+                                <td><?php echo $i ?></td>
+                                <td><?php echo $row['category']?></td>
+                                <td><?php echo $row['order_number']?></td>
+                                <td></td>
+                            </tr>
+                            <?php
+                                $i++;
+                                } } else { ?>
+                                <tr>
+                                    <td colspan="5">No data found</td>
 
-                            ?>
-                        <tr>
-                            <td><?php echo  $i?></td>
-                            <td><? echo $row['category']?></td>
-                            <td><? echo $row['order_number']?></td>
-                            <td></td>
+                                </tr>
 
-                        </tr>
-                        <?php
-                            $i++;
-                            } } else { ?>
-                            <td colspan="5">No Data Found</td>
-
-
-                        <?php
-                        } ?>
-
-                        </tbody>
-                    </table>
+                            <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<?php include ('footer.php');?>
+<?php include('footer.php'); ?>
