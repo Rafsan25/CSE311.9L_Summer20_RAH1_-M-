102d28d7f447ca56b912eea0d211db78771cc954
syed-reza98 <syed.reza@northsouth.edu>
2020-09-17 19:38:42 +0600

    style.css added add_link and grid_box

--- category.php
+++ category.php
@@ -31,7 +31,7 @@ $res=mysqli_query($con,$sql);
 ?>
     <div class="card">
         <div class="card-body">
-            <h1 class="grid_title">Category Master</h1>
+            <h1 class="grid_title">Category Manage</h1>
             <a href="manage_category.php" class="add_link">Add Category</a>
             <div class="row grid_box">
 
