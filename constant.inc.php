abe5d0038b22bd1bbb71aa009fd26faa5db34e53
syed-reza98 <syed.reza@northsouth.edu>
2020-09-18 16:16:48 +0600

    Added file for front page

---
+++ constant.inc.php
@@ -0,0 +1,10 @@
+<?php
+//C:\xampp\htdocs\CSE311.9L_Summer20_RAH1_-M-\media\dish
+define('SITE_NAME','Food Ordering Admin');
+define('FRONT_SITE_NAME','The Online Food Ordering');
+define('SERVER_IMAGE',$_SERVER['DOCUMENT_ROOT']."/php/food_ordering/");
+define('SERVER_DISH_IMAGE',SERVER_IMAGE."media/dish/");
+
+define('SITE_IMAGE',"http://127.0.0.1/php/food_ordering/");
+define('SITE_DISH_IMAGE',SITE_IMAGE."media/dish/");
+?>
