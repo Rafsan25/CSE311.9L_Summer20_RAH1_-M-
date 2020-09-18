39f192d5d126e239c69edb91332e81bf84ba3991
syed-reza98 <syed.reza@northsouth.edu>
2020-09-18 16:47:24 +0600

    addaed constant.inc.php file for img uploading but not tested yet

--- constant.inc.php
+++ constant.inc.php
@@ -1,10 +1,13 @@
 <?php
+
+// Not tested yet.
+
 //C:\xampp\htdocs\CSE311.9L_Summer20_RAH1_-M-\media\dish
 define('SITE_NAME','Food Ordering Admin');
 define('FRONT_SITE_NAME','The Online Food Ordering');
-define('SERVER_IMAGE',$_SERVER['DOCUMENT_ROOT']."/php/food_ordering/");
-define('SERVER_DISH_IMAGE',SERVER_IMAGE."media/dish/");
+define('SERVER_IMAGE',$_SERVER['DOCUMENT_ROOT']);
+define('SERVER_DISH_IMAGE',SERVER_IMAGE."/media/dish/");
 
-define('SITE_IMAGE',"http://127.0.0.1/php/food_ordering/");
+define('SITE_IMAGE',"http://127.0.0.1/CSE311.9L_Summer20_RAH1_-M-/");
 define('SITE_DISH_IMAGE',SITE_IMAGE."media/dish/");
 ?>
