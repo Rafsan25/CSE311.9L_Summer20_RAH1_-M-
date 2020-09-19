31b274341e0ae66ad40b335d03b454b42581b8a5
syed-reza98 <syed.reza@northsouth.edu>
2020-09-20 00:35:25 +0600

    Added Header and Footed for Front Site. Included other config and functions in index.php. Added title by defined front site name

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
-
+include ('config.php');
+include ('function.inc.php');
+include('constant.inc.php');
 ?>
 <!doctype html>
 <html class="no-js" lang="zxx">
 <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>The Online Food Ordering</title>
+        <title><?php echo FRONT_SITE_NAME ?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="assets/css/bootstrap.min.css">
         <link rel="stylesheet" href="assets/css/animate.css">
