2dc896999c9cab0fce932a705c7403de6803a741
syed-reza98 <syed.reza@northsouth.edu>
2020-09-19 18:40:37 +0600

    Index style

--- index.php
+++ index.php
@@ -19,22 +19,22 @@
     <body>
         <div class="slider-area">
             <div class="slider-active owl-dot-style owl-carousel">
-                <div class="single-slider pt-210 pb-220 bg-img" style="background-image:url(assets/img/slider/slider-1.jpg);">
+                <div class="single-slider pt-210 pb-220 bg-img" style="background-image:url(assets/img/slider/s1.jpg);">
                     <div class="container">
                         <div class="slider-content slider-animated-1">
-                            <h1 class="animated">Drink & Heathy Food</h1>
-                            <h3 class="animated">Fresh Heathy and Organic.</h3>
+                            <h1 class="animated">Order Food Online</h1>
+                            <h3 class="animated">The Natural Choice</h3>
                             <div class="slider-btn mt-90">
                                 <a class="animated" href="product-details.html">Order Now</a>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="single-slider pt-210 pb-220 bg-img" style="background-image:url(assets/img/slider/slider-2.jpg);">
+                <div class="single-slider pt-210 pb-220 bg-img" style="background-image:url(assets/img/slider/s2.jpg);">
                     <div class="container">
                         <div class="slider-content slider-animated-1">
-                            <h1 class="animated">Drink & Heathy Food</h1>
-                            <h3 class="animated">Fresh Heathy and Organic.</h3>
+                            <h1 class="animated">Order Food Online</h1>
+                            <h3 class="animated">The Natural Choice</h3>
                             <div class="slider-btn mt-90">
                                 <a class="animated" href="product-details.html">Order Now</a>
                             </div>
